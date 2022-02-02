efd03bb5a842859d9691001753b9934ea0e915c9
Martin <mart256@gmail.com>
2022-02-02 21:54:45 +0100

    Tail call optimization examples

--- sandbox.hs
+++ sandbox.hs
@@ -8,3 +8,10 @@ crearLista x acc = if x <= 0
         crearLista z (z:acc)
         where 
             z = x - 1
+
+sumarPares x suma = if x <= 0
+    then suma
+    else
+        if (mod x 2) == 0
+            then sumarPares (x - 2) (suma + x)
+            else sumarPares (x - 1) suma
